6dd5e9cb346c24aba9423a07bf4208553980406c
chenhu <chenhu1008@gmail.com>
2017-03-30 15:14:33 +0800

    refacotr the agent server

--- jdk6/src/main/java/com/jsits/training/jdk6/jmx/agent/RunServer.java
+++ jdk6/src/main/java/com/jsits/training/jdk6/jmx/agent/RunServer.java
@@ -16,6 +16,28 @@ public class RunServer {
     private static final Logger log = LoggerFactory.getLogger(RunServer.class.getName());
 
     public static void main(String... strings) throws Exception {
+        RunServer.mainMBean();
+    }
+    
+    /**
+     * MBean example
+     * @param strings
+     * @throws Exception
+     */
+    public static void mainMBean() throws Exception {
+        MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("com.jsits.training.mbeans:type=SysConfig");
+        SysConfigMBean configBean = new SysConfig();
+        mbeanServer.registerMBean(configBean, name);
+        log.debug("Waiting forever ... ...");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+    /**
+     * MXBean example
+     * @param strings
+     * @throws Exception
+     */
+    public static void mainMXBean() throws Exception {
         MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
         ObjectName name = new ObjectName("com.jsits.training.mbeans:type=SysConfig");
         SysConfigMBean configBean = new SysConfig();
